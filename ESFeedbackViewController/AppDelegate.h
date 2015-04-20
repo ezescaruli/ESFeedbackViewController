3e862829be158f97834bf8d39c0911a817a0ba9d
Ezequiel Scaruli <es@papercloud.com.au>
2015-04-20 17:37:18 +1000

    Initial Commit

---
+++ ESFeedbackViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ESFeedbackViewController
+//
+//  Created by Ezequiel Scaruli on 4/20/15.
+//  Copyright (c) 2015 Ezequiel Scaruli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
