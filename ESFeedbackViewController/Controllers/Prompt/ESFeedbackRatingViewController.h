3409aec569f9bd0c4ee1057b79cd6741887485fb
Ezequiel Scaruli <es@papercloud.com.au>
2015-04-21 11:57:44 +1000

    Adding `ESFeedbackRatingViewController`.

---
+++ ESFeedbackViewController/Controllers/Prompt/ESFeedbackRatingViewController.h
@@ -0,0 +1,11 @@
+//
+//  ESFeedbackRatingViewController.h
+//
+//  Created by Ezequiel Scaruli on 4/9/15.
+//
+
+#import "ESFeedbackPromptViewController.h"
+
+@interface ESFeedbackRatingViewController : ESFeedbackPromptViewController
+
+@end
---
+++ ESFeedbackViewController/Controllers/Prompt/ESFeedbackRatingViewController.m
@@ -0,0 +1,73 @@
+//
+//  ESFeedbackRatingViewController.m
+//
+//  Created by Ezequiel Scaruli on 4/9/15.
+//
+
+#import "ESFeedbackRatingViewController.h"
+#import "ESFeedbackViewController.h"
+#import "ESFeedbackNavigationController.h"
+
+#import "UIColor+Hex.h"
+
+
+@interface ESFeedbackRatingViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *textLabel;
+
+@end
+
+
+@implementation ESFeedbackRatingViewController
+
+
+#pragma mark - UIViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupTextLabel];
+}
+
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.textLabel.preferredMaxLayoutWidth = self.textLabel.bounds.size.width;
+}
+
+
+#pragma mark - Override methods
+
+
+- (NSString *)textForCancelButton {
+    return @"No, thanks";
+}
+
+
+- (NSString *)textForOKButton {
+    return @"Okay";
+}
+
+
+- (void)performCancelAction {
+    ESFeedbackNavigationController *navigationController = (ESFeedbackNavigationController *) self.navigationController;
+    [navigationController finish];
+}
+
+
+- (void)performOKAction {
+    ESFeedbackNavigationController *navigationController = (ESFeedbackNavigationController *) self.navigationController;
+    [navigationController goToAppStore];
+}
+
+
+#pragma mark - Private methods
+
+
+- (void)setupTextLabel {
+    self.textLabel.font = [ESFeedbackViewController textFont];
+    self.textLabel.textColor = [UIColor colorWithHex:0x595250];
+}
+
+
+@end
